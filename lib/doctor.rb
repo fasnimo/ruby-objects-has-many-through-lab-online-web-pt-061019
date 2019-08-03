4624f70fbf7d6205be51e09a9244cdd13932dd41
Rene Cocom <fasnimo@gmail.com>
2019-08-03 03:03:22 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -18,7 +18,7 @@ class Doctor
     end
   end
 
-  def new_appointment(patient,date)
+  def new_appointment(patient, date)
     patient = Patient.new(name)
     date = Appointment.new(date, patient, self)
     self.new_appointment
